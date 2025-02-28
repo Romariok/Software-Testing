35e6efb99b6787ce9f4161b2882e45a22e86e8b1
ta4ilka69 <artem_balin_2004@mail.ru>
2025-02-28 14:55:45 +0300

    unused import

--- lab1/src/test/java/itmo/st/MathUtilsTest.java
+++ lab1/src/test/java/itmo/st/MathUtilsTest.java
@@ -1,6 +1,5 @@
 package itmo.st;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import itmo.st.math.MathUtils;
